ae5825b3df1a2e934f96322e01bc2a854b997c18
danielmateos99 <63020032+danielmateos99@users.noreply.github.com>
2020-05-05 20:55:21 +0200

    lab3

---
+++ PoseNetOSC-Unity-master/Assets/Scripts lab3/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(20, 20, 20) * Time.deltaTime);
+    }
+}
